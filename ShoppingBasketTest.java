7a805583f8d1a8d36c2f986dd0a73734888b65f7
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-06-11 15:04:03 +0100

    can remove item extra test

--- ShoppingBasketTest.java
+++ ShoppingBasketTest.java
@@ -26,4 +26,17 @@ public class ShoppingBasketTest {
     assertEquals(0, basket.countItems());
   }
 
+  @Test
+  public void canRemoveSecondItem() {
+    ShoppingBasket basket = new ShoppingBasket();
+    Item item = new Item("peas", 100);
+    Item item2 = new Item("rice", 150);
+    Item item3 = new Item("pizza", 550);
+    basket.addItem(item);
+    basket.addItem(item2);
+    basket.addItem(item3);
+    basket.removeItem(1);
+    assertEquals(2, basket.countItems());
+  }
+
 }
