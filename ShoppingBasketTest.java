b768992f00eab44d4b6076589ca787975ef1a894
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-06-11 14:45:57 +0100

    count basket items

--- ShoppingBasketTest.java
+++ ShoppingBasketTest.java
@@ -1,9 +1,12 @@
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import shoppingbasket.*;
-//
-// public class ShoppingBasketTest {
-//
-//
-//
-// }
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ShoppingBasketTest {
+
+  @Test
+  public void checkStartsEmpty() {
+    ShoppingBasket basket = new ShoppingBasket();
+    assertEquals(0, basket.countItems());
+  }
+
+}
--- shoppingBasket.java
+++ shoppingBasket.java
@@ -1,7 +1,16 @@
-// package shoppingbasket;
-// import java.util.*;
-//
-// public class ShoppingBasket {
-//
-//
-// }
+import java.util.*;
+
+public class ShoppingBasket {
+
+  ArrayList<Item>contents;
+
+  public ShoppingBasket() {
+    this.contents = new ArrayList<Item>();
+  }
+
+  public int countItems() {
+    return this.contents.size();
+  }
+
+
+}
